64c1221f05f278d237c397cd0ae001f99fefe383
kwakjaehun <jh5414092@icloud.com>
2024-06-12 23:31:54 +0900

    Type : Feat
    네비게이션바 수정

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,20 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import api from "../../api/api";
-import { StNav } from "./NavigationBar.styled";
+import {
+  StButton,
+  StCategory,
+  StCategoryItem,
+  StDiv,
+  StNav,
+  StProfile,
+  StTitle,
+} from "./NavigationBar.styled";
 
 export const NavigationBar = () => {
   const navigate = useNavigate();
+
   const logOut = () => {
     localStorage.removeItem("accessToken");
     api.setAccessToken("");
     alert("로그아웃되었습니다.");
-    navigate("/auth/login");
+    navigate("/login");
   };
+
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => api.auth.getUser(),
+  });
 
   return (
     <StNav>
-      NavigationBar
-      <button onClick={logOut}>LOGOUT</button>
+      <StDiv>
+        <StTitle>나만의 가계부</StTitle>
+        <StCategory>
+          <StCategoryItem
+            onClick={() => {
+              navigate("/home");
+            }}
+          >
+            HOME
+          </StCategoryItem>
+          <StCategoryItem
+            onClick={() => {
+              navigate("/my-page");
+            }}
+          >
+            MY PAGE
+          </StCategoryItem>
+        </StCategory>
+      </StDiv>
+      <StDiv>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <StProfile>
+            <img src={user?.avatar} alt="" />
+            <h3>{user?.nickname}</h3>
+          </StProfile>
+        )}
+        <StButton onClick={logOut}>LOGOUT</StButton>
+      </StDiv>
     </StNav>
   );
 };
--- src/components/NavigationBar/NavigationBar.styled.js
+++ src/components/NavigationBar/NavigationBar.styled.js
@@ -5,6 +5,63 @@ export const StNav = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
+  height: 60px;
   background-color: #333;
   color: #fff;
+`;
+
+export const StButton = styled.button`
+  padding: 10px 20px;
+  height: 36px;
+  background-color: #fff;
+  color: #333;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #eee;
+  }
+`;
+
+export const StTitle = styled.h1`
+  font-size: 24px;
+  margin: 0;
+  display: flex;
+  align-items: center;
+`;
+
+export const StCategory = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const StCategoryItem = styled.button`
+  padding: 10px 20px;
+  background-color: ${(props) => (props.$active ? "#fff" : "#333")};
+  color: ${(props) => (props.$active ? "#333" : "#fff")};
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const StDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const StProfile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  img {
+    overflow: hidden;
+    object-fit: cover;
+    height: 48px;
+    width: 48px;
+  }
+  h3 {
+    font-size: 24px;
+    font-weight: 500;
+  }
 `;
