2bf424e9ddb8a9a3c2ca3fe4a1e6757f2d4e5fe5
kwakjaehun <jh5414092@icloud.com>
2024-06-12 23:24:33 +0900

    Type : Feat
    redux 작업

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import spendingListSlice from "./spendingListSlice";
+import userSlice from "./userSlice";
 
 const store = configureStore({
   reducer: {
     spendingList: spendingListSlice.reducer,
+    user: userSlice.reducer,
   },
 });
 
